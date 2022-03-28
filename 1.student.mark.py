b2449626d57ef371f4d170988d2e894988554674
hoangtran9901 <74967434+hoangtran9901@users.noreply.github.com>
2022-03-28 18:51:31 +0700

    Add files via upload

--- 1.student.mark.py
+++ 1.student.mark.py
@@ -1,64 +1,46 @@
-def getStudentCount ():
-    return int (input ("Enter number of students: "))
-    
-def getCourseCount ():
-    return int (input ("Enter number of courses: "))
-    
-def getCourseDetails ():
-    cid = input ("Enter course id: ")
-    cname = input ("Enter course name: ")
-    return cid, cname
-    
-def getStudentDetails ():
-    sid = input ("Enter student id: ")
-    sname = input ("Enter student name: ")
-    sdob = input ("Enter student's date of birth: ")
-    return sid, sname, sdob 
-    
-def getMarks (sid, cid):
-    prompt = f"Enter marks for student {sid} for course {cid}: ".format (sid, cid)
-    input (prompt)
-    
-nStudents = getStudentCount ()
-studentLst = []
-for i in range (nStudents):
-    sid, nm, dob = getStudentDetails ()
-    studentLst.append ((sid, nm, dob))
+numStundent = int (input ("Number of students: "))
+studentlist = []
+for i in range (numStundent):
+    studentid = input ("Student id: ")
+    studentname = input ("Student name: ")
+    studentbirth = input ("Student's date of birth: ")
+    studentlist.append (( studentid, studentname, studentbirth))
 
-nCourses = getCourseCount ()
-courseLst = []
-for i in range (nCourses):
-    cid, cname = getCourseDetails ()
-    courseLst.append ((cid, cname))
+coursnum = int (input ("Number of courses: "))
+courselist = []
+for i in range (coursnum):
+    coursid = input ("Course id: ")
+    coursname = input ("Course name: ")
+    courselist.append ((coursid,coursname))
     
-d = {}    
-n = int (input ("Enter how many student-course marks do you want to enter? "))
+tupl = {}    
+n = int (input ("Student-course marks ! "))
 for i in range (n):
     while True:
-        sid = input ("Enter student id: ")
-        cid = input ("Enter course id: ")
-        if sid not in [student [0] for student in studentLst]:
-            print ("Bad student id")
+        studentid = input ("Student id: ")
+        coursid = input ("Course id: ")
+        if studentid not in [student [0] for student in studentlist]:
+            print ("Wrong student id")
             continue 
-        if cid not in [course [0] for course in courseLst]:
-            print ("Bad course id")
+        if coursid not in [course [0] for course in courselist]:
+            print ("Wrong course id")
             continue 
         break
-    marks = int (input ("Enter marks: "))
-    if cid in d:
-        d [cid].append ((sid, marks))
+    marks = int (input ("Marks: "))
+    if coursid in tupl:
+        tupl [coursid].append ((studentid, marks))
     else:
-        d [cid] = [(sid, marks)]
+        tupl [coursid] = [(studentid, marks)]
         
-print ("\nListing all students now..")
-for s in studentLst:
-    print (f"Student id: {s[0]} Name: {s[1]} Date of birth: {s[2]}")
 
-print ("\nListing all courses now..")
-for c in courseLst:
-    print (f"Course id: {c[0]} Name: {c[1]}")
+for a in studentlist:
+    print (f"Student id: {a[0]} Name: {a[1]} Date of birth: {a[2]}")
+
+
+for b in courselist:
+    print (f"Course id: {b[0]} Name: {b[1]}")
     
-cid = input ("\nWhich course do you want to see all student marks for? ")
-if cid in d:
-    for tups in d [cid]:
+coursid = input ("\nEnter course! ")
+if coursid in tupl:
+    for tups in tupl [coursid]:
         print (f"Student {tups[0]} got {tups [1]} marks")
