71e61506143acb90e560b1e1e1276b19b67323da
hoangtran9901 <74967434+hoangtran9901@users.noreply.github.com>
2022-04-04 18:36:08 +0700

    Add files via upload

--- 2.student.mark.oop.py
+++ 2.student.mark.oop.py
@@ -4,63 +4,77 @@ class Student:
         self.sid = input('enter student rollno:')
         self.dob = input('enter student dob:')
         self.count=input('enter number of student:')
-    def name(self):  
-        return self.sname
-        
-    def id(self):  
-        return self.id
-    
-    def birthday(self):  
-        return self.dob
-    def number(self): 
-        return self.count
-        
+    def inputStudentCount():
+        count = int(input("Number of students:"))
+        return count
+
+    def inputStudentInfo(studentCount):
+    #returns a list students , with info from keyboard
+        students = []
+    # inout infoL  id , name , dob
+        for i in range(0, studentCount):
+            id = input("Student ID: ")
+            name = input("Student name: ")
+            dob = input("Student DoB: ")
+            student = {
+            "id": id,
+            "name": name,
+            "dob":dob,
+            "marks":{}
+        }
+        students.append(student)
+        return students
     
 class Course:
     def __init__(self):
         self.course_name =input('enter course name:')
         self.course_id = input('enter course id:')
         self.coursenum = input('enter number of course:')
-    def coursename(self):  
-        return self.course_name
-    def courseid(self):  
-        return self.course_id
-    def numcourse(self):
-        return self.coursenum
+    def inputCourseCount():
+        count = int(input("Number of courses:"))
+        return count
+
+    def inputCourseInfo(courseCount):
+      courses = []
+    # inout infoL  id , name , dob
+      for i in range(0, courseCount):
+        id = input("course ID: ")
+        name = input("course name: ")
+        
+        course = {
+            "id": id,
+            "name": name
+            
+        }
+        courses.append(course)
+        return courses
+
     
 
-class Mark:
-    def __init__(self):
+class Mark(Student,Course):
+    def __init__(self,student,course):
         self.__mark = input("enter mark")
     
-
-numStudents = Student()
-numStudents.number()
-Studentlist = []
-for i in range (numStudents):
-    studentname, studentid, dob = Student()
-    studentname.name()
-    studentid.id()
-    dob.birthday()
-    Studentlist.append ((studentname, studentid, dob))
-
-
-
-
-
-
-numCourse = Course()
-numCourse.numcourse()
-Courselist = []
-for i in range (numCourse):
-    courseid, coursename= Course()
-    courseid.courseid()
-    coursename.coursename()
-    
-    Courselist.append ((courseid,coursename))
-
-
-st1=Student()
-
     
 
+#enter student count and information
+studentCount = Student()
+students = Student(studentCount)
+students.inputStudentCount
+
+#enter course count and information
+courseCount = Course()
+courses = Course(courseCount)
+courses.inputCourseCount
+
+#select course, input mark for students in the given course
+#courseid = selectCourse(courses)
+#inputMark(courseid, students)
+
+
+
+
+# show marks for a given course
+
+#courseid = selectCourse(courses)
+#showMark(courseid, students)
