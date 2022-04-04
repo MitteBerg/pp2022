be37042a67256033c6a1b780e70b1210100a80d9
hoangtran9901 <74967434+hoangtran9901@users.noreply.github.com>
2022-04-04 19:17:13 +0700

    Add files via upload

--- 2.student.mark.oop.py
+++ 2.student.mark.oop.py
@@ -11,7 +11,7 @@ class Student:
     def inputStudentInfo(self,studentCount):
     #returns a list students , with info from keyboard
         students = []
-    # inout infoL  id , name , dob
+    
         for i in range(0, studentCount):
             id = self.sid
             name = self.sname
@@ -25,6 +25,11 @@ class Student:
         students.append(student)
         return students
     
+    def listStudents(students):
+        print("\n All student list")
+    for student in inputStudentInfo.students:
+        print(f"{student['id']: <10} {student['name']: <20} {student['dob']: <15}")
+    
 class Course:
     def __init__(self):
         self.course_name =input('enter course name:')
@@ -36,7 +41,7 @@ class Course:
 
     def inputCourseInfo(self):
       courses = []
-    # inout infoL  id , name , dob
+    
       for i in range(0, courseCount):
         id = self.course_id
         name = self.course_name
@@ -48,14 +53,25 @@ class Course:
         }
         courses.append(course)
         return courses
+    
+    def listCourses(courses):
+        print("\n All courses list")
+    for course in inputCourseInfo.courses:
+        print(f"{course['id']: <10} {course['name']: <20} ")
+    pass
 
     
 
 class Mark(Student,Course):
-    def __init__(self,student,course):
+    def __init__(self,stu,cou):
         self.__mark = input("enter mark")
+        self.students = stu.students
+        self.courseid = cou.courseid
         
-    def showmark():
+    def showMark():
+        print("\n All marks for the course {courseid}")
+    #for student in students:
+        #print(f"{student['name']: <20} {student['mark'][courseid]}")
         pass
     
     def GPA():
@@ -66,12 +82,11 @@ class Mark(Student,Course):
 #enter student count and information
 studentCount = Student()
 students = Student(studentCount)
-students.inputStudentCount
-
+students.inputStudentInfo
 #enter course count and information
 courseCount = Course()
 courses = Course(courseCount)
-courses.inputCourseCount
+courses.inputCourseInfo
 
 #select course, input mark for students in the given course
 #courseid = selectCourse(courses)
